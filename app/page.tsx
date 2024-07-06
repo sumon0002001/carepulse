d4c0ace6dbcbd171ad2b58645a40ac84628a6d8e
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-07-07 00:41:58 +0600

    add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="font-3xl underline">Helo world</h1>
+      <h1 className="text-white text-3xl underline">Helo world</h1>
+      <Button variant="outline">Click me</Button>
     </div>
   );
 }
