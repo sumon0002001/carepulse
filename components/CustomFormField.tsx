0edd06d1b58b6a5ddd68bea0da3c962fbda957ef
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-07-14 02:21:52 +0600

    done with registration form ui

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -12,6 +12,8 @@ import { Input } from "@/components/ui/input";
 import { Control } from "react-hook-form";
 import { FormFieldType } from "./forms/PatientForm";
 import Image from "next/image";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 
 interface CustomProps {
   control: Control<any>;
@@ -62,6 +64,20 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
             />
           </FormControl>
         </div>
+      );
+    case FormFieldType.PHONE_INPUT:
+      return (
+        <FormControl>
+          <PhoneInput
+            defaultCountry="BD"
+            placeholder={props.placeholder}
+            international
+            withCountryCallingCode
+            value={field.value as E164Number | undefined}
+            onChange={field.onChange}
+            className="input-phone"
+          />
+        </FormControl>
       );
   }
 };
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -3,10 +3,13 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Button } from "@/components/ui/button";
+import { useState } from "react";
 import { Form } from "@/components/ui/form";
 
 import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -18,26 +21,40 @@ export enum FormFieldType {
   SKELETON = "skeleton",
 }
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
-
 const PatientForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof UserFormValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values);
+
+    setIsLoading(true);
+    try {
+      const user = {
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+      };
+      // You can save user to your database here.
+      const newUser = await createUser(user);
+      console.log("New user created:", newUser);
+      if (newUser) {
+        router.push(`/patients/${newUser.id}/register`);
+      }
+    } catch {
+      console.error("Failed to submit form");
+    }
   }
   return (
     <Form {...form}>
@@ -71,7 +88,7 @@ const PatientForm = () => {
           label="phone number"
           placeholder="017192743879"
         />
-        <Button type="submit">Get Started</Button>
+        <SubmitButton isLoading={isLoading}>Get Start</SubmitButton>
       </form>
     </Form>
   );
